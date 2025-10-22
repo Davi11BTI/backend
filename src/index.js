8a669fe74488fc9b619f1cc3047b2e75cc97da7e
davievangelistasampaio <davievangelistasampaio@gmail.com>
2025-10-22 23:48:16 +0000

    bagulho novo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { response } from "express"
 import cors from "cors"
 
 import { persons } from "./persons.js"
@@ -7,12 +7,22 @@ const app = express()
 const port = 3333
 
 app.use(cors())
-
+app.use(express.json())
 
 app.get("/", (request, response) =>{
     response.json(persons)
 })
 
+app.post("/cadastrar", (request, response) => {
+    //desestruturação
+    const { user } = request.body
+    console.log(user)
+
+    //cadastro no banco de dados
+
+    response.status(201).json({ message : "Usuário cadastrado com sucesso!" })
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}!`)
-})
+}) 
