e71c8e737d088b721e2e35bb0943e1cf4a6b48da
davievangelistasampaio <davievangelistasampaio@gmail.com>
2025-11-05 23:59:42 +0000

    ultima atividade backends e bancs de dadis

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
-import express, { response } from "express"
+import express from "express"
 import cors from "cors"
+import mysql2 from "mysql2"
 
-import { persons } from "./persons.js"
+import {persons} from "./persons.js"
+
+const {DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
 
 const app = express()
+
 const port = 3333
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (request, response) =>{
+app.get("/", (request, response)=> {
     response.json(persons)
 })
 
-app.post("/cadastrar", (request, response) => {
-    //desestruturação
+app.post("/cadastrar", (request, response) =>{
     const { user } = request.body
     console.log(user)
 
-    //cadastro no banco de dados
 
-    response.status(201).json({ message : "Usuário cadastrado com sucesso!" })
+
+    const insertCommand = `
+        INSERT INTO pedrohenrique_02mb(name, email, password)
+        VALUES (?, ?, ?)
+    `
+
+    database.query(insertCommand, [user.name, user.email, user.password], (error) =>{
+        if(error){
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
+    })
+
+    
 })
 
+
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}!`)
-}) 
+    console.log(`Server Running on port ${port}`)
+})
+
+const database = mysql2.createPool({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    connectionLimit: 10
+})
